36fbd83c1d0f5f7e653148548236ecc70d9a9eee
antonderegt <antonderegt@gmail.com>
2015-05-14 12:39:34 +0200

    Added Panorama view

---
+++ DomoticaApp/DomoticaApp/PanoramaPage1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace DomoticaApp
+{
+    public partial class PanoramaPage1 : PhoneApplicationPage
+    {
+        public PanoramaPage1()
+        {
+          InitializeComponent();
+        }
+    }
+    /*
+    public partial class SensorDataPage : PhoneApplicationPage
+    {
+        public SensorDataPage()
+        {
+         //   InitializeComponent();
+        }
+    }
+
+    public partial class SettingsPage : PhoneApplicationPage
+    {
+        public SettingsPage()
+        {
+         //   InitializeComponent();
+        }
+    }
+    */
+}
