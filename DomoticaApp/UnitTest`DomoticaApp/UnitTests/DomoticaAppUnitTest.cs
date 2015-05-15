ea01b4585bed0edc8f40e1d50bb3b1f0093b2d98
antonderegt <antonderegt@gmail.com>
2015-05-15 22:36:51 +0200

    Added Unit Test and Refactored old tests

--- DomoticaApp/UnitTest`DomoticaApp/UnitTests/DomoticaAppUnitTest.cs
+++ DomoticaApp/UnitTest`DomoticaApp/UnitTests/DomoticaAppUnitTest.cs
@@ -16,7 +16,7 @@ namespace UnitTestDomoticaApp.UnitTests  {
     
     [TestMethod]  
     [Description("Check to see if AddIntegers works as desired")]      
-    public void AddIntegersTestShouldPass()          {  
+    public void AddIntegers_TwoAndThree_ShouldReturnFive()          {  
     //Should Pass Since we are using Assert.IsTrue and 2+3=5    
         var c=DomoticaApp.MainPage.AddIntegers(2, 3);        
         Assert.IsTrue(c == 5);       
@@ -24,10 +24,17 @@ namespace UnitTestDomoticaApp.UnitTests  {
     
     [TestMethod]      
     [Description("Check to see if AddIntegers works as desired")]   
-    public void AddIntegersTestShouldPass2()          {  
+    public void AddIntegers_TwoAndThree_ShouldNotReturnSeven()          {  
         //Should Pass since we are using Assert.IsFalse and 2+3 does not equal 7  
         var c=DomoticaApp.MainPage.AddIntegers(2, 3);            
         Assert.IsFalse(c == 7);        
-    }    
+    }
+    [TestMethod]
+    [Description("Check to see if AddIntegers works as desired")]
+    public void AddIntegers_ZeroAndMinusOne_ShouldResultMinusOne()
+    {
+        var c = DomoticaApp.MainPage.AddIntegers(0, -1);
+        Assert.IsTrue(c == -1);
+    } 
 }
 }
